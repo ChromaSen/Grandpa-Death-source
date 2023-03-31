9e92618f68deb08cf8c3194aeb721ccddb04b1ce
GDD <60884102+GsDrunkestDriver@users.noreply.github.com>
2023-03-31 16:27:37 +0100

    adaptive health bar colours
    hahaaaaaaaaaa

--- source/gameObjects/userInterface/ClassHUD.hx
+++ source/gameObjects/userInterface/ClassHUD.hx
@@ -73,7 +73,7 @@ class ClassHUD extends FlxTypedGroup<FlxBasic>
 
 		healthBar = new FlxBar(healthBarBG.x + 4, healthBarBG.y + 4, RIGHT_TO_LEFT, Std.int(healthBarBG.width - 8), Std.int(healthBarBG.height - 8));
 		healthBar.scrollFactor.set();
-		healthBar.createFilledBar(0xFFFF0000, 0xFF66FF33);
+		healthBar.createFilledBar(CoolUtil.getDominantIconColour(PlayState.dadOpponent.curCharacter), CoolUtil.getDominantIconColour(PlayState.boyfriend.curCharacter));
 		// healthBar
 		add(healthBar);
 
--- source/meta/CoolUtil.hx
+++ source/meta/CoolUtil.hx
@@ -2,6 +2,8 @@ package meta;
 
 import lime.utils.Assets;
 import meta.state.PlayState;
+import flixel.util.FlxColor;
+import openfl.display.BitmapData;
 
 using StringTools;
 
@@ -100,5 +102,63 @@ class CoolUtil
 			dumbArray.push(i);
 		}
 		return dumbArray;
+	}
+
+	/**
+	 * Gets the most frequent colour in a health icon.
+	 * @param char The character to get the icon for.
+	 * @return The most frequent FlxColor in the icon.
+	 */
+	 public static function getDominantIconColour(char:String)
+	{
+		//start by cleaning up the char name so it matches the file name, getting rid of any hyphens and whatever comes after them
+		/*if (char.contains('-'))
+			char = char.split('-')[0];*/ //unneeded probably
+			
+		//grab the icon's bitmapdata
+		var iconBitmapData:BitmapData = BitmapData.fromFile('assets/images/icons/icon-$char.png');
+		//set up a dictionary to store the colours and their frequencies
+		var colourDict:Map<Int, Int> = new Map<Int, Int>();
+		//loop through the icon's pixels while ignoring any black or transparent pixels
+		for (x in 0...iconBitmapData.width)
+		{
+			for (y in 0...iconBitmapData.height)
+			{
+				var pixelColour:Int = iconBitmapData.getPixel32(x, y);
+				if (pixelColour != 0 && pixelColour != 0xFF000000)
+				{
+					//if the colour is already in the dictionary, increment its frequency
+					if (colourDict.exists(pixelColour))
+					{
+						colourDict.set(pixelColour, colourDict.get(pixelColour) + 1);
+					}
+					//if the colour isn't in the dictionary, add it with a frequency of 1
+					else
+					{
+						colourDict.set(pixelColour, 1);
+					}
+				}
+			}
+		}
+		//set up vars for the most frequent colour and its frequency
+		var mostFrequentColour:Int = 0;
+		var mostFrequentColourFrequency:Int = 0;
+		//loop through the dictionary
+		for (colour in colourDict.keys())
+		{
+			//if the current colour's frequency is higher than the most frequent colour's frequency, set the most frequent colour to the current colour
+			if (colourDict.get(colour) > mostFrequentColourFrequency)
+			{
+				mostFrequentColour = colour;
+				mostFrequentColourFrequency = colourDict.get(colour);
+			}
+		}
+		//throw in some traces for debugging
+		trace('$char most frequent colour: $mostFrequentColour');
+		trace('$char most frequent colour frequency: $mostFrequentColourFrequency');
+		//convert the most frequent colour to an FlxColor
+		var dominantColour:FlxColor = FlxColor.fromInt(mostFrequentColour);
+		//return the most frequent colour
+		return dominantColour;
 	}
 }
